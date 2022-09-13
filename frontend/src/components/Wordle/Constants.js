6db0ed71ae85165145cf4050a2b02390bcf8b47e
Michael Lee <vlmlee2@gmail.com>
2022-09-12 23:33:58 -0500

    clarified constants

--- frontend/src/components/Wordle/Constants.js
+++ frontend/src/components/Wordle/Constants.js
@@ -1,6 +1,10 @@
-const UNSOLVED = "UNSOLVED";
-const WRONG_POSITION = "WRONG_POSITION";
-const SOLVED = "SOLVED";
-const UNUSED = "UNUSED";
+// Solve states
+const UNSOLVED = "UNSOLVED"; // grey
+const WRONG_POSITION = "WRONG_POSITION"; // yellow
+const SOLVED = "SOLVED"; // green
 
-export default {SOLVED, WRONG_POSITION, UNSOLVED, UNUSED};
+// Keyboard states
+const UNUSED_LETTER = "UNUSED_LETTER"; // User has not used letter yet
+const NOT_PRESENT_IN_SOLUTION = "NOT_PRESENT_IN_SOLUTION"; // User has used this letter, and it is not in the solution
+
+export default {SOLVED, WRONG_POSITION, UNSOLVED, UNUSED_LETTER, NOT_PRESENT_IN_SOLUTION};
