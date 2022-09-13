c72a06745108297c553ded9f35a15d1dd52a9abf
Michael Lee <vlmlee2@gmail.com>
2022-09-13 00:26:28 -0500

    Updated stylings

--- frontend/src/components/Wordle/Keyboard.js
+++ frontend/src/components/Wordle/Keyboard.js
@@ -8,7 +8,25 @@ const KEYS = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
 export default function Keyboard(keyboardState) {
     return <div className={"keyboard"}>{KEYS.map((row, i) =>
         <div key={i} className={"keyboard-row"}>
-            {row.split("").map((letter, j) => <div key={j} className={"keyboard-key"}>{letter}</div>)}
+            {row.split("").map((letter, j) => {
+                if (letter === "Z") {
+                    return <>
+                        <div className={"keyboard-key keyboard-key-enter keyboard-key-UNUSED_LETTER"}>ENTER</div>
+                        <div key={j} className={"keyboard-key keyboard-key-UNUSED_LETTER"}>{letter}</div>
+                    </>;
+                }
+                
+                if (letter === "M") {
+                    return <>
+                        <div key={j} className={"keyboard-key keyboard-key-UNUSED_LETTER"}>{letter}</div>
+                        <div className={"keyboard-key keyboard-key-backspace keyboard-key-UNUSED_LETTER"}>
+                            <img src = "backspace-svgrepo-com.svg" alt="svg"/>
+                        </div>
+                    </>;
+                }
+                
+                return <div key={j} className={"keyboard-key keyboard-key-UNUSED_LETTER"}>{letter}</div>;
+            })}
         </div>)
     }</div>;
 }
