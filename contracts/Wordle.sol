83ec3aabc0217dcc5ddc7bfde4f328ecf96ac8e0
Michael Lee <vlmlee2@gmail.com>
2022-09-23 12:32:37 -0500

    Updated wordle contract to include verify guess and position

--- contracts/Wordle.sol
+++ contracts/Wordle.sol
@@ -11,7 +11,7 @@ contract Wordle {
     address public leaderboard;
     uint256 public accumulator;
     uint256 public modulus;
-    uint256[] public proofs;
+    uint256[] public proofs; // [ 1µ, 2µ, 3µ, 4µ, 5µ, ...(2-5µ, depending on the word) ] where µ = letter
     uint256 public wordlePuzzleNo = 0; // updated daily
 
     // attempts[wordlePuzzleNo][user] => number of attempts by user at wordle puzzle number
@@ -27,7 +27,7 @@ contract Wordle {
         wordlePuzzleNo++;
     }
 
-    function createNewWordleSecret(uint256 _accumulator, uint256 _modulus, uint256[] memory proofs) external {
+    function createNewWordlePuzzle(uint256 _accumulator, uint256 _modulus, uint256[] memory proofs) external {
         require(msg.sender == owner);
 
         accumulator = _accumulator;
@@ -36,7 +36,22 @@ contract Wordle {
         resetAttempts();
     }
 
-    function verifyGuess(uint8 index, uint256 guess) external returns (bool) {
+    // Checks if the letter is in the solution set.
+    // The wordle proofs will always map:
+    // [ 1µ, 2µ, 3µ, 4µ, 5µ, ...(3-5µ, depending on the word) ]
+    function verifyGuess(uint256 guess) external returns (bool) {
+        for (uint8 i = 4; i < proofs.length; i++) {
+            uint256 memory proof = proofs[i];
+            if (proof**guess == accumulator%modulus) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Checks if the letter is in the correct position.
+    function verifyPosition(uint8 index, uint256 guess) external returns (bool) {
         uint256 memory proof = proofs[index]; // proofs[index] = G**[Set \ value@index] % modulus
 
         if (proof**guess == accumulator%modulus) {
