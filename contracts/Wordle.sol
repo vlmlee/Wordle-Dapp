ee22fe0b1b224dc86575b5c0ff3298b372f68bc1
Michael Lee <vlmlee2@gmail.com>
2022-09-25 07:50:24 -0500

    Fixed types. Refactored to fix hard coded array lengths. Added log2 function, need to test to see if it actually works as expected.

--- contracts/Wordle.sol
+++ contracts/Wordle.sol
@@ -85,7 +85,7 @@ contract Wordle is Leaderboard {
 
     function fastModExp(uint8 base, uint256 exponent, uint256 modulus) pure internal returns (int result) {
         require(exponent < 1024);
-        uint256[] binaryArr = intToBinary(exponent);
+        uint8[] binaryArr = intToBinary(exponent);
         uint256[] baseModExpArr = divideAndConquer(base, binaryArr, modulus);
 
         for (int8 i = 0; i < baseModExpArr.length; i++) {
@@ -98,8 +98,8 @@ contract Wordle is Leaderboard {
     }
 
     // Dynamic programming to prevent expensive exponentiation
-    function divideAndConquer(uint256 base, uint256[] memory binaryArr, uint256 modulus) pure internal returns (uint256[]) {
-        uint256 memory memo = new uint8[](binaryArr.length);
+    function divideAndConquer(uint256 base, uint8[] memory binaryArr, uint256 modulus) pure internal returns (uint256[]) {
+        uint256 memory memo = new uint256[](binaryArr.length);
         memo[0] = (base ** 1) % modulus;
 
         for (int8 i = 1; i < binaryArr.length; i++) {
@@ -113,16 +113,51 @@ contract Wordle is Leaderboard {
         return memo;
     }
 
+    // Outputs to binary array in a "little-endian"-like way, i.e. 30 = [0, 1, 1, 1, 1]
     function intToBinary(uint8 n) pure internal returns (uint8[]) {
         require(n < 1024);
 
-        uint8[] output = new uint8[](10);
+        uint8 binLength = log2(n);
 
-        for (uint8 i = 0; i < 10; i++) {
-            output[9 - i] = (n % 2 == 1) ? 1 : 0;
+        uint8[] output = new uint8[](binLength);
+
+        for (uint8 i = 0; i < binLength; i++) {
+            output[binLength - i] = (n % 2 == 1) ? 1 : 0;
             n /= 2;
         }
 
         return output;
+    }
+
+    // Should use an external library for security.
+    function log2(uint x) returns (uint y) {
+        assembly {
+            let arg := x
+            x := sub(x,1)
+            x := or(x, div(x, 0x02))
+            x := or(x, div(x, 0x04))
+            x := or(x, div(x, 0x10))
+            x := or(x, div(x, 0x100))
+            x := or(x, div(x, 0x10000))
+            x := or(x, div(x, 0x100000000))
+            x := or(x, div(x, 0x10000000000000000))
+            x := or(x, div(x, 0x100000000000000000000000000000000))
+            x := add(x, 1)
+            let m := mload(0x40)
+            mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
+            mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
+            mstore(add(m,0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
+            mstore(add(m,0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
+            mstore(add(m,0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
+            mstore(add(m,0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
+            mstore(add(m,0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
+            mstore(add(m,0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
+            mstore(0x40, add(m, 0x100))
+            let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
+            let shift := 0x100000000000000000000000000000000000000000000000000000000000000
+            let a := div(mul(x, magic), shift)
+            y := div(mload(add(m,sub(255,a))), shift)
+            y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
+        }
     }
 }
