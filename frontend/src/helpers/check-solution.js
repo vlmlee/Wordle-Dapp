883187b10a002d1cb9da0be800c297e8c3ddc62f
Michael Lee <vlmlee2@gmail.com>
2022-10-16 08:21:46 -0500

    prettifier

--- frontend/src/helpers/check-solution.js
+++ frontend/src/helpers/check-solution.js
@@ -1,19 +1,20 @@
-import Constants from "./Constants";
+import Constants from './Constants';
 
-const wordBank = require("./wordBank.json");
+const wordBank = require('./wordBank.json');
 
-const solution = ["r", "e", "c", "a", "p", "0r", "1e", "2c", "3a", "4p"];
+const solution = ['r', 'e', 'c', 'a', 'p', '0r', '1e', '2c', '3a', '4p'];
 
 function isWordInWordBank(word) {
-    return wordBank.includes(word);
+    return wordBank.includes(word.toLowerCase());
 }
 
 function checkSolution(currentState) {
-    const checkedSolution = currentState.map(letter => {
+    const checkedSolution = currentState.map((letter) => {
         if (solution.includes(letter.value)) {
-            const attemptContainsMultiple = currentState.filter(l => l.value === letter.value).length > 1;
-            const solutionContainsMultiple = solution.filter(s => s.length > 1 && s.includes(letter.value)).length > 1;
-    
+            const attemptContainsMultiple = currentState.filter((l) => l.value === letter.value).length > 1;
+            const solutionContainsMultiple =
+                solution.filter((s) => s.length > 1 && s.includes(letter.value)).length > 1;
+
             if (solution.includes(letter.position + letter.value)) {
                 return {
                     position: letter.position,
@@ -34,18 +35,15 @@ function checkSolution(currentState) {
                 };
             }
         }
-    
+
         return {
             position: letter.position,
             value: letter.value,
             solveState: Constants.NOT_PRESENT_IN_SOLUTION
         };
     });
-    
+
     return checkedSolution;
 }
 
-export {
-    isWordInWordBank,
-    checkSolution
-};
+export { isWordInWordBank, checkSolution };
