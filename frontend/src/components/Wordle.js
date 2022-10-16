18a2e767112aaf2f73b9f69dec1f35f86c45a0fe
Michael Lee <vlmlee2@gmail.com>
2022-10-16 03:35:34 -0500

    Updated Wordle.js to use reducer states

--- frontend/src/components/Wordle.js
+++ frontend/src/components/Wordle.js
@@ -1,46 +1,58 @@
-import React, {useEffect} from "react";
-import Letter from "./Letter";
-import FauxRow from "./FauxRow";
-import "../stylesheets/Wordle.scss";
+import React, { useEffect } from 'react';
+import Letter from './Letter';
+import FauxRow from './FauxRow';
+import '../stylesheets/Wordle.scss';
 
-export default function Wordle({previousStates, currentState, updateLetter, attempts}) {
-    let attemptsLeft = 5 - attempts;
-    
-    const generatedPreviousAttempts = React.useMemo(() => {
-        return previousStates.map((states, index) =>
-            <div key={index}>
-                {states.map((letter, j) =>
-                    <Letter key={j}
-                            position={letter.position}
-                            solveState={letter.solveState}
-                            value={letter.value} />)}
-            </div>
-        );
-    }, [attempts]);
-    
-    const generateDivs = () => {
-        let arr = [];
-        for (let i = 0; i < attemptsLeft; i++) {
-            arr.push(<FauxRow key={i} />);
-        }
-        return arr;
-    };
-    
-    return <div className="wordle">
-        {previousStates.length > 0 && <div className={"wordle-attempts"}>
-            {generatedPreviousAttempts()}
-        </div>}
-        <div className={"wordle-current-state"}>
-            {currentState.map((letter, index) =>
-                <Letter key={index}
+export default function Wordle({
+    previousAttempts,
+    currentAttempt,
+    updateLetter,
+    attemptNumber
+}) {
+    let attemptsLeft = 5 - attemptNumber;
+
+    const generatedPreviousAttempts = () => {
+        return previousAttempts.map((_attempts, i) => (
+            <div key={`previous-attempts__row-${i}`}>
+                {_attempts.map((letter, j) => (
+                    <Letter
+                        key={`previous_attempts__element-${j}`}
                         position={letter.position}
                         solveState={letter.solveState}
                         value={letter.value}
-                        updateLetter={updateLetter}/>
+                    />
+                ))}
+            </div>
+        ));
+    };
+
+    const generateEmptyRows = () => {
+        let arr = [];
+        for (let i = 0; i < attemptsLeft; i++) {
+            arr.push(<FauxRow key={`empty-rows-${i}`} />);
+        }
+        return arr;
+    };
+
+    return (
+        <div className="wordle">
+            {previousAttempts.length > 0 && (
+                <div className={'previous-attempts'}>
+                    {generatedPreviousAttempts()}
+                </div>
             )}
+            <div className={'current-attempt'}>
+                {currentAttempt.map((letter, i) => (
+                    <Letter
+                        key={`current-attempt__letter-${i}`}
+                        position={letter.position}
+                        solveState={letter.solveState}
+                        value={letter.value}
+                        updateLetter={updateLetter}
+                    />
+                ))}
+            </div>
+            <div className={'empty-rows'}>{generateEmptyRows()}</div>
         </div>
-        <div className={"wordle-faux-state"}>
-            {generateDivs()}
-        </div>
-    </div>;
+    );
 }
