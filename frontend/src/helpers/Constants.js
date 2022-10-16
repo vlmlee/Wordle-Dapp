2170b702cac08b14754c7e117632b0e8ebd20dee
Michael Lee <vlmlee2@gmail.com>
2022-10-16 02:47:11 -0500

    Linter says that I should wrap exported members within a named object variable

--- frontend/src/helpers/Constants.js
+++ frontend/src/helpers/Constants.js
@@ -1,10 +1,18 @@
 // Solve states
-export const UNSOLVED = "UNSOLVED"; // grey
-export const WRONG_POSITION = "WRONG_POSITION"; // yellow
-export const SOLVED = "SOLVED"; // green
+export const UNSOLVED = 'UNSOLVED'; // grey
+export const WRONG_POSITION = 'WRONG_POSITION'; // yellow
+export const SOLVED = 'SOLVED'; // green
 
 // Keyboard states
-export const UNUSED_LETTER = "UNUSED_LETTER"; // User has not used letter yet
-export const NOT_PRESENT_IN_SOLUTION = "NOT_PRESENT_IN_SOLUTION"; // User has used this letter, and it is not in the solution
+export const UNUSED_LETTER = 'UNUSED_LETTER'; // User has not used letter yet
+export const NOT_PRESENT_IN_SOLUTION = 'NOT_PRESENT_IN_SOLUTION'; // User has used this letter, and it is not in the solution
 
-export default { SOLVED, WRONG_POSITION, UNSOLVED, UNUSED_LETTER, NOT_PRESENT_IN_SOLUTION };
+const CONSTANTS = {
+    SOLVED,
+    WRONG_POSITION,
+    UNSOLVED,
+    UNUSED_LETTER,
+    NOT_PRESENT_IN_SOLUTION
+};
+
+export default CONSTANTS;
