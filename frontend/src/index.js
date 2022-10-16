e75cc31fd2cb5b4cef7c2c4b86541858d678213f
Michael Lee <vlmlee2@gmail.com>
2022-10-16 01:47:22 -0500

    Added react query provider at the top level

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Dapp } from "./components/Dapp";
+import Dapp from "./Dapp";
 import {createRoot} from "react-dom/client";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
-// We import bootstrap here, but you can remove if you want
 import "bootstrap/dist/css/bootstrap.css";
 
-// This is the entry point of your application, but it just renders the Dapp
-// react component. All of the logic is contained in it.
+const queryClient = new QueryClient()
+
 const root = createRoot(  document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Dapp />
+      <QueryClientProvider client={queryClient}>
+        <Dapp />
+      </QueryClientProvider>
   </React.StrictMode>,
 );
