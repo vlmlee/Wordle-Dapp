4923f99e66b8bc82d07637f589ca3a60d199bc0b
Michael Lee <vlmlee2@gmail.com>
2022-10-17 11:17:01 -0500

    Removed react-query (unneeded)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Dapp from './Dapp';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 import 'bootstrap/dist/css/bootstrap.css';
-
-const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Dapp />
-        </QueryClientProvider>
+        <Dapp />
     </React.StrictMode>
 );
