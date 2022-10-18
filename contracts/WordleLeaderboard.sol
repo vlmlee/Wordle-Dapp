9d961a65c8467725296515d1534ca45157e6718b
Michael Lee <vlmlee2@gmail.com>
2022-10-18 04:39:07 -0500

    Removed unused contracts

--- contracts/WordleLeaderboard.sol
+++ contracts/WordleLeaderboard.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 
-import "./Leaderboard.sol";
+contract WordleLeaderboard {
 
-contract WordleLeaderboard is Leaderboard {
-
-    constructor(bytes32 _name, uint256 _endTime) Leaderboard(_name, _endTime) {
+    constructor(bytes32 _name, uint256 _endTime) {
 
     }
 }
